379f7f24cb820de5a2c279cd692fb8d410c98c9b
JEGOUZ <jrjegouz@gmail.com>
2026-08-23 18:33:30 +0200

    update AOUT

--- index.js
+++ index.js
@@ -1099,8 +1099,8 @@ function buildLeaderboardEmbed({
 return (
   `\n> **${idx + 1}**   <@${id}> ${rankEmoji ? rankEmoji + ' ' : ''}${badges}` +
   `   •   **${data.rr || 0}** ʀʀ` +
-  `   •   <:VIDE:1472667823875559708>${invites}` +
-  `   •   <:VIDE:1493378253446975619>${timeouts}` +
+  `   •   ${invites}<:VIDE:1472667823875559708>` +
+  `   •   ${timeouts}<:VIDE:1493378253446975619>` +
   `\n> ${bar}`
 );
 });
