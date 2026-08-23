2bad6dfc11840e7f06dfc0310cf476cfcd2c283e
JEGOUZ <jrjegouz@gmail.com>
2026-08-23 18:31:52 +0200

    update AOUT

--- index.js
+++ index.js
@@ -1099,7 +1099,6 @@ function buildLeaderboardEmbed({
 return (
   `\n> **${idx + 1}**   <@${id}> ${rankEmoji ? rankEmoji + ' ' : ''}${badges}` +
   `   •   **${data.rr || 0}** ʀʀ` +
-  `   •   ${winrate}%ᴡʀ` +
   `   •   <:VIDE:1472667823875559708>${invites}` +
   `   •   <:VIDE:1493378253446975619>${timeouts}` +
   `\n> ${bar}`
