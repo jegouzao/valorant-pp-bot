a1677967b7b3ffbd9c568926613f0de45d609725
JEGOUZ <jrjegouz@gmail.com>
2026-08-23 21:12:24 +0200

    update AOUT

--- index.js
+++ index.js
@@ -1093,10 +1093,10 @@ function buildLeaderboardEmbed({
     }
 
 return (
-  `\n- **#${idx + 1}**   <@${id}> ${rankEmoji ? rankEmoji +'':''}${badges}` +
-  `   •   **${data.rr || 0}** <:VIDE:1541125087384829962>`+
-  `  •   **${invites}**<:VIDE:1472667823875559708>` +
-  `   •   **${timeouts}**<:VIDE:1493378253446975619>` +
+  `\n- **#${idx + 1}**  ●  <@${id}> ${rankEmoji ? rankEmoji +'':''}${badges}` +
+  `  ●  **${data.rr || 0}** <:VIDE:1541125087384829962>`+
+  ` ●  **${invites}**<:VIDE:1472667823875559708>` +
+  `  ●  **${timeouts}**<:VIDE:1493378253446975619>` +
   `\n-# <:VIDE:1465704930160410847>${bar}\n`
 );
 });
