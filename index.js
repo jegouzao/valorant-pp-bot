6f9a82719ea32dc062f94afa906a4c17c6ff611a
JEGOUZ <jrjegouz@gmail.com>
2026-08-30 15:32:55 +0200

    migration Containers V2

--- index.js
+++ index.js
@@ -1166,7 +1166,7 @@ function buildLeaderboardContainer({
       new TextDisplayBuilder().setContent(
         `## <:VIDE:1493046347337699499> LEADERBOARD ꜱᴇᴘᴛᴇᴍʙʀᴇ\n` +
         `-# ᴅᴇʀɴɪᴇʀᴇ ᴍɪꜱᴇ ᴀ ᴊᴏᴜʀ : <t:${Math.floor(Date.now() / 1000)}:R>\n` +
-        `-# ᴊᴏᴜᴇᴜʀꜱ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ : \`${playerCount}\`` +
+        `-# ᴊᴏᴜᴇᴜʀꜱ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛꜱ : \`${playerCount}\`\n` +
         `-# ᴄᴀꜱʜᴘʀɪᴢᴇ ᴅᴜ ᴍᴏɪꜱ : ...`
 
       )
