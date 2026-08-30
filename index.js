f819467d569d85dd95262fb1fd4ca01d043d5b9e
JEGOUZ <jrjegouz@gmail.com>
2026-08-30 02:41:49 +0200

    migration Containers V2

--- index.js
+++ index.js
@@ -1593,12 +1593,12 @@ const statsContainer = new ContainerBuilder()
   .addTextDisplayComponents(
     new TextDisplayBuilder().setContent(
       `### <:VIDE:1493266536813690970> #${position}　` +
-      `${stats.rr}<:VIDE:1541125087384829962>　` +
-      `${winrate}<:VIDE:1541167342535319603>%` +
-      `${stats.games} <:VIDE:1472667851239456935>　` +
-      `${stats.wins} <:VIDE:1493266372954820741>　` +
-      `${invitesData.invites} <:VIDE:1472667823875559708>　` +
-      `${stats.timeouts || 0} <:VIDE:1493378253446975619>　` +
+      `${stats.rr}<:VIDE:1541125087384829962> ` +
+      `${winrate}<:VIDE:1541167342535319603>%　` +
+      `${stats.games}<:VIDE:1472667851239456935>　` +
+      `${stats.wins}<:VIDE:1493266372954820741>　` +
+      `${invitesData.invites}<:VIDE:1472667823875559708>　` +
+      `${stats.timeouts || 0}<:VIDE:1493378253446975619>　\n` +
       `-# ${progressBar}`
     )
   )
