fb8678774ee250bb18299309b71df47a93a56ff3
JEGOUZ <jrjegouz@gmail.com>
2026-08-30 14:28:13 +0200

    migration Containers V2

--- index.js
+++ index.js
@@ -1146,8 +1146,8 @@ function buildLeaderboardContainer({
     }
 
     return (
-  ` **#${idx + 1}** <@${id}> ${rankEmoji ? rankEmoji + ' ' : ''}${badges}\n` +
-  ` ${data.rr || 0}<:VIDE:1541125087384829962>` +
+  ` **#${idx + 1}** <@${id}> ${rankEmoji ? rankEmoji + ' ' : ''}${badges} ` +
+  ` **•** **${data.rr || 0}**<:VIDE:1541125087384829962>` +
   ` **•** **${winrate}**<:VIDE:1541167342535319603>**%**` +
   ` **•** **${games}**<:VIDE:1472667851239456935>` +
   ` **•** **${wins}**<:VIDE:1493266372954820741>` +
